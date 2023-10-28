fcdd17420b39781fa83cacdf4cd733c1fc3207be
Haeryz00 <muh4mm4dh4r1z@gmail.com>
2023-10-28 20:36:20 +0700

    Added detail page for items

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -23,6 +23,7 @@ from core.views import index, contact
 
 urlpatterns = [
     path('', index, name='index'),  # Use an empty string to match the root URL
+    path('item/', include('item.urls', namespace='item')),  # Use the app's namespace
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
